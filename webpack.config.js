e021ea6d02fc265514e1a0e3c41f4aa800ee5aea
doozles411 <cindy.phelps01@gmail.com>
2022-10-14 12:07:46 -0700

    Import html webpack plugin and configure

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    devtool: 'inline-source-map',
     entry: './src/index.ts',
+    plugins: [
+        new HtmlWebpackPlugin()
+    ],
     module: {
         rules: [
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
             {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
@@ -16,6 +25,7 @@ module.exports = {
     },
     output: {
         filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: 'assets/[hash][ext][query]'
     }
 };
