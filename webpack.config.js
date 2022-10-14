42efffd864dc0e78de5881975fe611d490d1a652
doozles411 <cindy.phelps01@gmail.com>
2022-10-14 14:10:48 -0700

    Change CopyPlugin patterns settings

--- webpack.config.js
+++ webpack.config.js
@@ -19,8 +19,8 @@ module.exports = {
         new CopyPlugin({
             patterns: [
                 {
-                    from: 'src/img',
-                    to: 'img'
+                    from: './src/assets',
+                    to: './assets'
                 }
             ]
         })
