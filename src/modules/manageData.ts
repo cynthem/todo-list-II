c73c736c3ebf1be68dc3754fec78ee4442962f65
doozles411 <cindy.phelps01@gmail.com>
2022-10-17 13:13:47 -0700

    Create and export manageData

---
+++ src/modules/manageData.ts
@@ -0,0 +1,15 @@
+export function manageData() {
+    function createTodo(title: string, details: string, dueDate: string, priority: string, project: string, checked = false) {
+        return {
+            title,
+            details,
+            dueDate,
+            priority,
+            project,
+            checked
+        };
+    }
+    return {
+        createTodo
+    }
+};
