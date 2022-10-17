e7fc10130daac6a6e443e51e3c76a7b7d0a8b5a9
doozles411 <cindy.phelps01@gmail.com>
2022-10-17 14:35:03 -0700

    Return loadPage

--- src/modules/UI.ts
+++ src/modules/UI.ts
@@ -1,1 +1,30 @@
 import { todos } from './todoList';
+
+export default (function UI() {
+
+    function loadPage() {
+        /*displayData.renderAllTodos(todos, listContainer);
+        displayData.renderProjectList(todos, listContainer); */
+        loadDashboard();
+        loadTodoList();
+    };
+
+    function loadDashboard() {
+        loadFilters();
+        loadProjects();
+    };
+
+    function loadTodoList() {
+        loadTodoItems();
+    };
+
+    function loadFilters() {};
+
+    function loadProjects() {};
+
+    function loadTodoItems() {};
+
+    return {
+        loadPage
+    };
+})();
