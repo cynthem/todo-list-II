cae9f3f4024bf7fd23687723fa020059d0eeb78f
doozles411 <cindy.phelps01@gmail.com>
2022-10-17 16:42:03 -0700

    Format UI functions

--- src/modules/UI.ts
+++ src/modules/UI.ts
@@ -4,14 +4,17 @@ import { currentDay } from '../util/dates';
 export default (function UI() {
 
     function loadPage() {
-        /*displayData.renderAllTodos(todos, listContainer);
-        displayData.renderProjectList(todos, listContainer); */
-        loadHeader();
         loadDashboard();
         loadTodoList();
     };
 
-    function loadHeader() {
+    function loadDashboard() {
+        toggleDashboard();
+        toggleFilters();
+        loadProjects();
+    };
+
+    function toggleDashboard() {
         const el1 = document.querySelector('.main-dashboard');
         const dashboard: Element = el1!;
         const el2 = document.querySelector('.fa-bars');
@@ -22,28 +25,45 @@ export default (function UI() {
         });
     };
 
-    function loadDashboard() {
-        loadFilters();
-        loadProjects();
-    };
-
-    function loadTodoList() {
-        loadTodoItems();
-    };
-
-    function loadFilters() {
+    function toggleFilters() {
         const filterBtns = document.querySelectorAll('.filters-btn');
         /* filterBtns.forEach(btn => {
     btn.addEventListener('click', e => changeDOM.manageTodosRender(e, todos, listContainer));
 }); */
     };
 
-    function loadProjects() {};
-
-    function loadTodoItems() {};
-
-    return {
-        loadPage,
-        loadHeader
+    function loadProjects() {
+        handleAddBtn();
+        toggleProjects();
+        handleEmptyProject();
     };
+
+    function handleAddBtn() {};
+
+    function toggleProjects() {};
+
+    function handleEmptyProject() {};
+
+    function loadTodoList() {
+        /*displayData.renderAllTodos(todos, listContainer);
+        displayData.renderProjectList(todos, listContainer); */
+        loadTodoItems();
+    };
+
+    function loadTodoItems() {
+        handleCheckbox();
+        handleNotesBtn();
+        handleEditBtn();
+        handleDeleteBtn();
+    };
+
+    function handleCheckbox() {};
+
+    function handleNotesBtn() {};
+
+    function handleEditBtn() {};
+
+    function handleDeleteBtn() {};
+
+    return { loadPage };
 })();
