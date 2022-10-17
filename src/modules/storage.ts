a2cbc7c347ff111e36d9f2ee0b4c9c2c7390cf4e
doozles411 <cindy.phelps01@gmail.com>
2022-10-17 13:09:22 -0700

    Push data to default todoList

--- src/modules/storage.ts
+++ src/modules/storage.ts
@@ -1,6 +1,35 @@
+import { format } from 'date-fns';
+
+/**
+ * 
+ * @returns a list of todos that are either already locally stored, or a default list
+ * 
+ */
 function getTodos() {
     let stored = localStorage.getItem('todos');
     let todoList;
+
+    /*Formatted dates for default todo list*/
+    const dateObject = new Date();
+    const month = format(dateObject, 'MM');
+    const day = format(dateObject, 'dd');
+    const year = format(dateObject, 'yyyy');
+    const today = `${month}-${day}-${year}`;
+    const weekPast = new Date(dateObject.getFullYear(), dateObject.getMonth(), dateObject.getDate() - 7);
+    const weekAgoMonth = format(weekPast, 'MM');
+    const weekAgoDay = format(weekPast, 'dd');
+    const weekAgoYear = format(weekPast, 'yyyy');
+    const weekAgo = `${weekAgoMonth}-${weekAgoDay}-${weekAgoYear}`;
+    const weekFuture = new Date(dateObject.getFullYear(), dateObject.getMonth(), dateObject.getDate() + 5);
+    const weekFutureMonth = format(weekFuture, 'MM');
+    const weekFutureDay = format(weekFuture, 'dd');
+    const weekFutureYear = format(weekFuture, 'yyyy');
+    const weekNext = `${weekFutureMonth}-${weekFutureDay}-${weekFutureYear}`;
+    const monthFuture = new Date(dateObject.getFullYear(), dateObject.getMonth(), dateObject.getDate() + 32);
+    const monthFutureMonth = format(monthFuture, 'MM');
+    const monthFutureDay = format(monthFuture, 'dd');
+    const monthFutureYear = format(monthFuture, 'yyyy');
+    const monthNext = `${monthFutureMonth}-${monthFutureDay}-${monthFutureYear}`;
 
     if (typeof stored === 'string') {
         todoList = JSON.parse(stored);
@@ -11,6 +40,10 @@ function getTodos() {
             'week': [],
             'Kitchen renovation': []
         }
+        todoList['Kitchen renovation'].push(manageData.createTodo('Remove vinyl floor', 'replace with subfloor', weekNext, 'high', 'Kitchen renovation'));
+        todoList['Kitchen renovation'].push(manageData.createTodo('Build cabinets', 'pre-painted cabinets only', weekAgo, 'low', 'Kitchen renovation', true));
+        todoList['Kitchen renovation'].push(manageData.createTodo('Install countertop', 'leave room for butcher block', monthNext, 'medium', 'Kitchen renovation'));
+        todoList.all.push(manageData.createTodo('Finish book for book club', 'let Angela borrow afterward', today, 'high', 'all'));
     }
     return todoList;
 }
