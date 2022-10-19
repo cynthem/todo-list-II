b5b8cc0ce61d14e55bfc2e029d05e85527948aa2
doozles411 <cindy.phelps01@gmail.com>
2022-10-18 18:00:40 -0700

    Complete toggleCheckedTodo

--- src/modules/displayData.ts
+++ src/modules/displayData.ts
@@ -203,7 +203,35 @@ export default (function displayData() {
         itemEdit.classList.toggle('selected');
     };
 
-    function toggleCheckedTodo(e: Event, todos: ProjectsObject, listContainer: Element) {};
+    function toggleCheckedTodo(e: Event, todos: ProjectsObject, listContainer: Element) {
+        const checkedTodo = (e.target as Element).parentElement!.parentElement;
+        const checkbox = (e.target as Element);
+
+        checkbox.classList.toggle('fa-square');
+        checkbox.classList.toggle('fa-square-check');
+
+        const todoItems = checkedTodo!.children;
+        const itemsLeft = todoItems[0];
+        const itemsRight = todoItems[1];
+        const itemTitle = itemsLeft.children[1];
+        const itemNotes = itemsRight.children[0];
+        const itemDate = itemsRight.children[1];
+        const itemEdit = itemsRight.children[2];
+
+        itemTitle.classList.toggle('selected');
+        itemTitle.classList.toggle('strike');
+        itemNotes.classList.toggle('done');
+        itemDate.classList.toggle('selected');
+        itemEdit.classList.toggle('selected');
+        
+        const item: number = Number(checkedTodo!.dataset.index);
+        const project: string = checkedTodo!.dataset.project!;
+        todos[project][item].checked = !todos[project][item].checked;
+
+        localStorage.setItem('todos', JSON.stringify(todos));
+        
+        renderProjectList(todos);
+    };
 
     return {
         renderFilters,
