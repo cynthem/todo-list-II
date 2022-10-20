efdc19207f28d2859c1715cc60d29b3a25729365
doozles411 <cindy.phelps01@gmail.com>
2022-10-19 18:54:14 -0700

    Remove unnecessary import

--- src/modules/UI.ts
+++ src/modules/UI.ts
@@ -1,7 +1,6 @@
-import { todos } from './todoList';
 import displayData from './displayData';
 import manageData from './manageData';
-import { ProjectsObject } from '../util/types';
+import { todos } from './todoList';
 import { currentDay } from '../util/dates';
 
 export default (function UI() {
