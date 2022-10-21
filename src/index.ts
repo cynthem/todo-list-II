f8a57fa0a079627cadbb649c53d01530f4f564db
doozles411 <cindy.phelps01@gmail.com>
2022-10-21 10:56:09 -0700

    Add click and submit event listeners

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@ import UI from './modules/UI';
 import './index.css';
 
 document.addEventListener('DOMContentLoaded', UI.loadPage);
+
+document.addEventListener('click', UI.loadPage);
+
+document.addEventListener('submit', UI.loadPage);
