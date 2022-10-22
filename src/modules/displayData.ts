2bc2b782fa45b4cdb752dc9f1a05230c4ac664ba
doozles411 <cindy.phelps01@gmail.com>
2022-10-22 16:52:54 -0700

    Change add-todo popup radio label references

--- src/modules/displayData.ts
+++ src/modules/displayData.ts
@@ -1024,11 +1024,11 @@ export default (function displayData() {
         const addNewMediumPriority: HTMLInputElement = addNewMediumPriorityEl!;
         const addNewHighPriorityEl = <HTMLInputElement>document.getElementById('new-todo-high');
         const addNewHighPriority: HTMLInputElement = addNewHighPriorityEl!;
-        const addLowLabelEl = <HTMLLabelElement>document.querySelector('.new-priority-label');
+        const addLowLabelEl = <HTMLLabelElement>document.querySelector('.new-priority-label-low');
         const addLowLabel: HTMLLabelElement = addLowLabelEl!;
-        const addMediumLabelEl = <HTMLLabelElement>document.querySelector('.new-priority-label');
+        const addMediumLabelEl = <HTMLLabelElement>document.querySelector('.new-priority-label-medium');
         const addMediumLabel: HTMLLabelElement = addMediumLabelEl!;
-        const addHighLabelEl = <HTMLLabelElement>document.querySelector('.new-priority-label');
+        const addHighLabelEl = <HTMLLabelElement>document.querySelector('.new-priority-label-high');
         const addHighLabel: HTMLLabelElement = addHighLabelEl!;
         
         const project = manageData.getSelectedProject();
